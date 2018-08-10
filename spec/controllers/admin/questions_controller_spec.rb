62987bc05d6b9c6eed64aa67d353e62ccfd3f74c
k-kibi <k.kibihara@xtone.co.jp>
2018-08-10 16:13:12 +0900

    add test code for admin

--- spec/controllers/admin/questions_controller_spec.rb
+++ spec/controllers/admin/questions_controller_spec.rb
@@ -1,26 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Admin::QuestionsController, type: :controller do
+  let(:question) { create :question }
 
   describe "GET #index" do
-    it "returns http success" do
+    before do
       get :index
-      expect(response).to have_http_status(:success)
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:successful)
     end
   end
 
   describe "GET #new" do
-    it "returns http success" do
+    before do
       get :new
-      expect(response).to have_http_status(:success)
+    end
+    it "returns http success" do
+      expect(response).to have_http_status(:successful)
     end
   end
 
   describe "GET #edit" do
+    before do
+      question
+      get :edit, params: { id: question.id }
+    end
+
     it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:successful)
     end
   end
 
+  describe "POST #create" do
+    let(:content) { 'Question text' }
+    let(:answer) { 'Answer' }
+    before do
+      post :create, params: { question: { content: content, answer: answer } }
+    end
+    subject { Question.order(created_at: :desc).last }
+
+    it "creates new question" do
+      is_expected.to have_attributes(content: 'Question text', answer: 'Answer')
+    end
+  end
+
+  describe "PUT #update" do
+    let(:content) { 'Updated question text' }
+    let(:answer) { 'Renewal answer' }
+    before do
+      question
+      put :update, params: { id: question.id, question: { content: content, answer: answer } }
+    end
+    subject { question.reload }
+
+    it 'updates question' do
+      is_expected.to have_attributes(content: 'Updated question text', answer: 'Renewal answer')
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before do
+      question
+      delete :destroy, params: { id: question.id }
+    end
+    subject { question.reload }
+
+    it 'deletes question' do
+      expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { create :question, content: 'content', answer: '5' }
+
+  describe 'validates' do
+    let(:question) { build :question }
+    subject { question.valid? }
+
+    it { is_expected.to be_truthy }
+
+    it 'content must be present' do
+      question.content = ''
+      is_expected.to be_falsey
+    end
+
+    it 'answer must be present' do
+      question.answer = ''
+      is_expected.to be_falsey
+    end
+  end
+
+  describe '#correct?' do
+    let(:users_answer) { '5' }
+    subject { question.correct?(users_answer) }
+
+    it { is_expected.to be_truthy }
+
+    context 'when answer is text' do
+      let(:users_answer) { 'five' }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when answer is wrong' do
+      let(:users_answer) { '6' }
+      it { is_expected.to be_falsey }
+    end
+  end
 end
