69c70c294705f477d03dfa82baf4e2d850e5e910
k-kibi <k.kibihara@xtone.co.jp>
2018-08-12 13:04:09 +0900

    Fix method correct?

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,10 +8,12 @@ class Question < ApplicationRecord
   # @param [String] users_answer
   # @return [TrueClass | FalseClass]
   def correct?(users_answer)
-    if /\A\d+\z/ =~ self.answer
-      self.answer == users_answer || self.answer.to_i.humanize == users_answer
-    else
-      self.answer == users_answer
+    return true if self.answer == users_answer
+
+    if /\d+/ =~ self.answer
+      return true if self.answer.gsub(/(\d+)/) { $1.to_i.humanize } == users_answer
     end
+
+    false
   end
 end
