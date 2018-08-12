5efd23366b9ac57725b121549c03d6433ff788ca
k-kibi <k.kibihara@xtone.co.jp>
2018-08-12 19:23:08 +0900

    modify test

--- spec/javascript/quiz.spec.jsx
+++ spec/javascript/quiz.spec.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 import { shallow, mount, render } from 'enzyme'
 import { Quiz, AnswerForm, AnswerFeedback } from 'packs/quiz'
 
 describe('<Quiz />', () => {
-  const questions = [
-    { id: 1, content: 'How many letters are there in the English alphabet?' }
-  ];
-
   test('Initial states', () => {
     const wrapper = shallow(<Quiz />);
     const questions = [
